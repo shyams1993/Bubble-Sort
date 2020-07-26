4882c8bee7e7a3d15d1eac90e1da62ddca749df5
Shyam Salil <shyamsalil1993@gmail.com>
2020-07-26 22:31:56 +0530

    Update BubbleSort.c

--- BubbleSort.c
+++ BubbleSort.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-void printer(int n, int nums[]);
-void main()
+void printer(int n, int array[]);
+int main(void)
 {
     int x = 0, n = 8, temp = 0;
     int nums[8] = {6, 3, 8, 5, 2, 7, 4, 1};
-    while (x < n)
+    while (x != n)
     {
         for (int y = 0; y < n-1; y++)
         {
@@ -20,10 +20,11 @@ void main()
     printer(n, nums);
 }
 
-void printer(int n, int nums[])
+void printer(int n, int array[])
 {
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i != n; i++)
     {
-        printf("%i ",nums[i]);
+        printf("%i ",array[i]);
     }
+    printf("\n");
 }
